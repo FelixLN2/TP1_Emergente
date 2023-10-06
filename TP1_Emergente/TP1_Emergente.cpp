9a468b9e989d30ca107d562aeba72a4b797377b6
FelixLN2 <2215307@cmontmorency.qc.ca>
2023-10-06 17:20:26 -0400

    Exercice1 #5

--- TP1_Emergente/TP1_Emergente.cpp
+++ TP1_Emergente/TP1_Emergente.cpp
@@ -21,9 +21,19 @@ void trierTab(int* tab, int taille) {
 }
 
 void afficherTab(int* tab, int taille) {
-    for (int i = 0; i < 10; ++i) {
-        std::cout << *(tab+i) << ", ";
+
+    cout << "tableau contient: ";
+    int* pointeur = tab;
+    for (int i = 0; i < taille; i++) {
+        cout << *pointeur;
+        if (i < taille - 1) {
+            std::cout << ", ";
+        }
+        pointeur++;
     }
+    cout << endl;
+
+    
 }
 
 bool egalite(int tab1[], int tab2[], int taille) {
@@ -71,28 +81,71 @@ bool egalitePtr(int* tab1, int* tab2, int taille)
     }
 }
 
+int* concatenerTab(int* tab1, int* tab2, int taille) {
+    int tailleNouvelle = taille + taille;
+
+    int* resultat = new int[tailleNouvelle];
+
+    for (int i = 0; i < taille; i++) {
+        resultat[i] = tab1[i];
+    }
+
+    for (int i = 0; i < taille; i++) {
+        resultat[taille + i] = tab2[i];
+    }
+    return resultat;
+
+}
+
 
 
 int main()
-
 {
     
     const int NB_ELEMENTS = 10;
     int tableau1[] = { 22, 44, 12, 61, 4, 99, 33, 17, 15, 123 };
     int tableau2[] = { 22, 33, 44, 61, 4, 99, 12, 17, 123, 15 };
     int tableau3[] = { 22, 44, 12, 61, 4, 99, 33, 17, 15, 123 };
-   
+    int* tableau4 = concatenerTab(tableau1, tableau2, NB_ELEMENTS);
 
-    trierTab(tableau1, NB_ELEMENTS);
-
+  
+    cout << "tableau1 : ";
+    afficherTab(tableau1, NB_ELEMENTS);
+    cout << " | tableau2 : ";
     afficherTab(tableau2, NB_ELEMENTS);
+    cout << " | tableau3 : ";
+    afficherTab(tableau3, NB_ELEMENTS);
+    cout << " | tableau4 : ";
+    afficherTab(tableau4, 20);
   
     
     string boolStr = egalite(tableau1, tableau2, NB_ELEMENTS) ? "true" : "false";
-    cout << boolStr;
-    string boolStr2 = egalitePtr(tableau1, tableau2, NB_ELEMENTS) ? "true" : "false";
-    cout << boolStr2;
+    cout << " | tableau1 et tableau2 Egaux? : "+boolStr;
+    string boolStr2 = egalite(tableau1, tableau3, NB_ELEMENTS) ? "true" : "false";
+    cout << " | tableau1 et tableau3 Egaux? : " + boolStr2;
+    string boolStr3 = egalitePtr(tableau1, tableau2, NB_ELEMENTS) ? "true" : "false";
+    cout << " | tableau1 et tableau2 Egaux? : " + boolStr3;
+    string boolStr4 = egalitePtr(tableau1, tableau3, NB_ELEMENTS) ? "true" : "false";
+    cout << " | tableau1 et tableau3 Egaux? : " + boolStr4;
     
+    trierTab(tableau1, NB_ELEMENTS);
+    trierTab(tableau2, NB_ELEMENTS);
+    trierTab(tableau3, NB_ELEMENTS);
+    trierTab(tableau4, 20);
+
+    cout << "tableau1 : ";
+    afficherTab(tableau1, NB_ELEMENTS);
+    cout << " | tableau2 : ";
+    afficherTab(tableau2, NB_ELEMENTS);
+    cout << " | tableau3 : ";
+    afficherTab(tableau3, NB_ELEMENTS);
+    cout << " | tableau4 : ";
+    afficherTab(tableau4, 20);
+
+    string boolStr5 = egalitePtr(tableau1, tableau2, NB_ELEMENTS) ? "true" : "false";
+    cout << " | tableau1 et tableau2 Egaux? : " + boolStr5;
+    string boolStr6 = egalitePtr(tableau1, tableau3, NB_ELEMENTS) ? "true" : "false";
+    cout << " | tableau1 et tableau3 Egaux? : " + boolStr6;
 }
 
 
